853e90e2db1e4b1c057c679e06f5a579b89a560d
Aziz Mahmud <azizmahmud2003@outlook.com>
2026-07-06 17:47:45 -0400

    Initial Commit

---
+++ Meditation+Manifestation App Watch App/Meditation_Manifestation_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meditation_Manifestation_AppApp.swift
+//  Meditation+Manifestation App Watch App
+//
+//  Created by Aziz Mahmud on 7/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Meditation_Manifestation_App_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Meditation+Manifestation AppTests/Meditation_Manifestation_AppTests.swift
@@ -0,0 +1,19 @@
+//
+//  Meditation_Manifestation_AppTests.swift
+//  Meditation+Manifestation AppTests
+//
+//  Created by Aziz Mahmud on 7/6/26.
+//
+
+import Testing
+@testable import Meditation_Manifestation_App
+
+struct Meditation_Manifestation_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
